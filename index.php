805c5c2a47fb2cb6070e85d7381e020108e837ac
shaikmalikbasha <shaikmalikbasha1998@gmail.com>
2020-11-21 17:26:04 +0530

    NDS-001, Welcome message

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>NDS - Nova Digital Solution</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+  <link href="assets/img/logo.png" rel="icon">
+  <link href="assets/img/logo.png" rel="apple-touch-icon">
+</head>
+
+<body>
+<div>
+  <marquee>
+    <h1>...Welcome to Nova Digital Solution...</h1>
+  </marquee>
+</div>
+</body>
+</html>
