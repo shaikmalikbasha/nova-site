f46775479c4805f1e015dcd0c4590a5e3049eaaf
shaikmalikbasha <shaikmalikbasha1998@gmail.com>
2025-12-20 09:20:03 +0530

    fix - icon.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
+function loadTags() {
+  let icons_url =
+    "https://shaikmalikbasha583.github.io/itz_me_mr_developer/icons.html";
+
+  fetch(icons_url)
+    .then((response) => response.text())
+    .then((data) => {
+      console.log("Icons loaded successfully");
+      document.getElementById("icons-list").innerHTML = data;
+      document.getElementById("icons-list-repeat").innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Error loading icons:", error);
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   // Initialize Sidenav
   M.Sidenav.init(document.querySelectorAll(".sidenav"));
   M.Parallax.init(document.querySelectorAll(".parallax"));
+  loadTags();
 
   // const body = document.body;
   // const toggleButton = document.getElementById("theme-toggle");
@@ -33,25 +50,3 @@ document.addEventListener("DOMContentLoaded", function () {
   //   setTheme(isDark);
   // });
 });
-
-function loadTags() {
-  let icons_url = "icons.html";
-
-  if (window.location.protocol === "file:") {
-    icons_url =
-      "https://shaikmalikbasha583.github.io/itz_me_mr_developer/icons.html";
-  }
-
-  fetch(icons_url)
-    .then((response) => response.text())
-    .then((data) => {
-      console.log("Icons loaded successfully");
-      document.getElementById("icons-list").innerHTML = data;
-      document.getElementById("icons-list-repeat").innerHTML = data;
-    })
-    .catch((error) => {
-      console.error("Error loading icons:", error);
-    });
-}
-
-loadTags();
